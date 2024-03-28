d0075d3e9bdbc305270d9d0789cb98b3221e58be
Ritwik Bhushan <ritwik.b2001@gmail.com>
2024-03-28 17:55:57 +0530

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,10 @@
+import React, {useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+const Profile = () => {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
